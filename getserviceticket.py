13a1cfda6f6bfd6c2d80ebd24d1905400d2db53e
weerachaiy <weerachai.y@rmutt.ac.th>
2018-06-07 19:08:06 +0700

    6th commit

--- getserviceticket.py
+++ getserviceticket.py
@@ -7,10 +7,10 @@ api.print_host()
 
 api.print_device()
 
-s_IP = input("Source IP: ")
-#s_IP = "10.1.15.117"
-d_IP = input("Destination IP: ")
-#d_IP = "10.2.1.22"
+#s_IP = input("Source IP: ")
+s_IP = "10.1.15.117"
+#d_IP = input("Destination IP: ")
+d_IP = "10.2.1.22"
 flow_url = api.print_flowid(s_IP,d_IP)
 print(flow_url)
 ticket = api.get_ticket()
