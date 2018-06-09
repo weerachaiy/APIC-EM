2353b9cf54c0ca8299de5bb977707148642af5f1
พ่อผมชื่อชํานาญ แย้มวจี <kitarotao@gmail.com>
2018-06-09 14:31:05 +0700

    test commit

--- api.py
+++ api.py
@@ -2,24 +2,26 @@ import requests
 import json
 import tabulate
 
-requests.packages.urllib3.disable_warnings()
+#requests.packages.urllib3.disable_warnings()
 
-api_base_url = "https://devnetsbx-netacad-apicem-1.cisco.com/api/v1"
-api_username = "devnetuser"
-api_password = "NTgmY5UY"
-api_base_url = "https://devnetsbx-netacad-apicem-2.cisco.com/api/v1"
-api_username = "devnetuser"
-api_password = "w0ISNW79"
-#api_base_url = "https://devnetsbx-netacad-apicem-3.cisco.com/api/v1"
+#api_url = "https://devnetsbx-netacad-apicem-1.cisco.com"
 #api_username = "devnetuser"
-#api_password = "Xj3BDqbU"
-#api_base_url = "https://SandBoxAPICEM.cisco.com/api/v1"
+#api_password = "NTgmY5UY"
+#api_url = "https://devnetsbx-netacad-apicem-2.cisco.com"
+#api_username = "devnetuser"
+#api_password = "w0ISNW79"
+api_url = "https://devnetsbx-netacad-apicem-3.cisco.com"
+api_username = "devnetuser"
+api_password = "Xj3BDqbU"
+#api_url = "https://SandBoxAPICEM.cisco.com"
 #api_username = "devnetuser"
 #api_password = "Cisco123!"
+api_base_url = api_url + "/api/v1"
 
 def get_ticket():
     headers = {"content-type": "application/json"}
     api_url = api_base_url + "/ticket"
+    print("REST API: " + api_url)
     payload = {"username": api_username,
                "password": api_password}
     response = requests.post(api_url, json=payload, headers=headers, verify=False).json()
@@ -29,6 +31,7 @@ def get_hosts(ticket):
     headers = {"content-type": "application/json",
                "X-AUTH-TOKEN": ticket}
     api_url = api_base_url + "/host"
+    print("REST API: " + api_url)
     response = requests.get(api_url, headers=headers, verify=False).json()
     return response["response"]
 
@@ -36,6 +39,7 @@ def get_devices(ticket):
     headers = {"content-type": "application/json",
                "X-AUTH-TOKEN": ticket}
     api_url = api_base_url + "/network-device"
+    print("REST API: " + api_url)
     response = requests.get(api_url, headers=headers, verify=False).json()
     return response["response"]
 
@@ -75,20 +79,67 @@ def print_device():
                     "Type"]
     print(tabulate.tabulate(device_list, table_header))
 
-def get_flowid(ticket, sourceIP, destIP):
+def get_flowurl(sourceIP, destIP):
+    ticket = get_ticket()
+    print("Service ticket is",ticket)
     headers = {"content-type": "application/json",
                "X-AUTH-TOKEN": ticket}
     payload = {"destIP": destIP,
                "sourceIP": sourceIP}
     api_url = api_base_url + "/flow-analysis"
+    print("REST API: " + api_url)
+    print("SRC IP: " + sourceIP)
+    print("DST IP: " + destIP)
     response = requests.post(api_url, json=payload, headers=headers, verify=False).json()
-    return response["response"]
+    print(response["response"]["url"])
+    return response["response"]["url"]
 
-def print_flowid(sourceIP, destIP):
+def print_flowurl(sourceIP, destIP):
+    flow_url = get_flowurl(sourceIP, destIP)
     ticket = get_ticket()
     print("Service ticket is",ticket)
-    flow = get_flowid(ticket, sourceIP, destIP)
-    headers = {"content-type": "application/json",
-               "X-AUTH-TOKEN": ticket}
-    api_base_url = "https://devnetsbx-netacad-apicem-2.cisco.com"
-    return api_base_url + flow["url"]
+    headers = {"content-type": "application/json","X-AUTH-TOKEN": ticket}
+    flow_url = api_url +  flow_url
+    print("REST API: " + flow_url)
+    #print("SRC IP: " + sourceIP)
+    #print("DST IP: " + destIP)
+    while True:
+        response = requests.get(flow_url, headers=headers, verify=False).json()
+        if response["response"]["request"]["status"] == "COMPLETED":
+            break
+        if response["response"]["request"]["status"] == "FAILED":
+            break
+        print(response["response"]["request"]["status"])
+    if response["response"]["request"]["status"] != "FAILED":
+        paths = response["response"]["networkElementsInfo"]
+    path_list =[]
+    i = 0
+    for path in paths:
+        i = i + 1
+        if "ip" in path:
+            path_ip = path["ip"]
+        else:
+            path_ip = "------"
+        if "name" in path:
+            path_name = path["name"]
+        else:
+            path_name = "------"
+        if "type" in path:
+            path_type = path["type"]
+        else:
+            path_type = "------"
+        if "linkInformationSource" in path:
+            link_type = path["linkInformationSource"]
+        else:
+            link_type = "Destination"
+        path_list.append([i,
+                          path_ip,
+                          path_name,
+                          path_type,
+                          link_type])
+    table_header = ["No.",
+                        "IP Addess",
+                        "Name",
+                        "Type",
+                        "Link"]
+    print(tabulate.tabulate(path_list, table_header))
--- getserviceticket.py
+++ getserviceticket.py
@@ -1,11 +1,21 @@
 import api
+import requests
+import json
+import tabulate
 
-api.print_host()
+def main():
+    requests.packages.urllib3.disable_warnings()
 
-api.print_device()
+    api.print_host()
 
-s_IP = input("Source IP: ")
-d_IP = input("Destination IP: ")
-flow_url = api.print_flowid(s_IP,d_IP)
-print(flow_url)
-#api.print_flow_detail(flow_url)
+    api.print_device()
+
+    #s_IP = input("Source IP: ")
+    s_IP = "10.1.15.117"
+    #d_IP = input("Destination IP: ")
+    d_IP = "10.2.1.22"
+
+    api.print_flowurl(s_IP,d_IP)
+
+if __name__ == '__main__':
+    main()
